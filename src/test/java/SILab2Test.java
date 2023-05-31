a4b357cf482bbbb9555eb5f0b19a72e5c8963db6
Bojan Popovski <bojpopovski2002@gmail.com>
2023-05-31 14:42:51 +0200

    Finish with tests

--- src/test/java/SILab2Test.java
+++ src/test/java/SILab2Test.java
@@ -1,24 +1,62 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SILab2Test {
-    private final SILab2 userT = new SILab2();
-
+    //Every Branch Tests
     @Test
     void exceptionTest(){
         RuntimeException ex;
-        ex = assertThrows(RuntimeException.class, () -> userT.function(null, null));
+        ex = assertThrows(RuntimeException.class, () -> SILab2.function(null, null));
         assertTrue(ex.getMessage().contains("Mandatory information missing!"));
     }
 
-    /*@Test
+    @Test
     void usernameTest(){
-        User user = new User(null, "boki_softversko", "smth@smth.com");
-        boolean ex = userT.function(user, null);
-        assertTrue(ex);
-    }*/
+        Assertions.assertEquals(true, SILab2.function(new User(null, "boki_softversko", "boki2@smth.com"), Collections.emptyList()));
+    }
+
+    @Test
+    void UsersTests() {
+        User u1 = new User("boki", "boki_softversko", "boki@smth.com");
+        List<User> users = new ArrayList<>();
+        users.add(u1);
+
+        Assertions.assertEquals(false, SILab2.function(new User("boki", "boki", "bokac@smth.com"), users));
+    }
+
+    @Test
+    void spaceTest(){
+        Assertions.assertEquals(false, SILab2.function(new User("boki", "boki finki", "bokac@smthcom"), Collections.emptyList()));
+    }
+
+    @Test
+    void specialCharacterTest(){
+        Assertions.assertEquals(false, SILab2.function(new User("boki", "bokisoftversko", "boki@smthcom"), Collections.emptyList()));
+    }
+
+    //Multiple Condition Tests
+    @Test
+    void multipleConditionTest(){
+        RuntimeException ex;
+        //TXX
+        ex=Assertions.assertThrows(RuntimeException.class, () -> SILab2.function(null, null));
+        Assertions.assertTrue(ex.getMessage().contains("Mandatory information missing!"));
+
+        //FTX
+        ex=Assertions.assertThrows(RuntimeException.class, () -> SILab2.function(new User("boki", null, "boki@smth.com"), null));
+        Assertions.assertTrue(ex.getMessage().contains("Mandatory information missing!"));
+
+        //FFT
+        ex=Assertions.assertThrows(RuntimeException.class, () -> SILab2.function(new User("boki", "boki_softversko", null), null));
+        Assertions.assertTrue(ex.getMessage().contains("Mandatory information missing!"));
+
+        //FFF
+        Assertions.assertEquals(false, SILab2.function(new User("boki", "boki_softversko", "boki@smth.com"), Collections.emptyList()));
+    }
 }
